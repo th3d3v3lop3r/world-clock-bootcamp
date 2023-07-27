6e3b55d46fb9f387c1eab563a270f2d18c511d76
th3d3v3lop3r <cjieren@gmail.com>
2023-07-27 20:34:01 +0800

    replaced App component render with tick function in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
