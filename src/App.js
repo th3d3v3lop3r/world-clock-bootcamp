2962e4c434f0b10a811737ceba29cae4ac0384e3
th3d3v3lop3r <cjieren@gmail.com>
2023-07-28 13:47:14 +0800

    moved tick function to Clock.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { date: new Date() };
+  // }
 
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
+  // tick() {
+  //   this.setState({
+  //     date: new Date(),
+  //   });
+  // }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
+  // componentDidMount() {
+  //   this.timerID = setInterval(() => this.tick(), 1000);
+  // }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+  // componentWillUnmount() {
+  //   clearInterval(this.timerID);
+  // }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.date.toLocaleTimeString()}</p>
+          {/* <p>{this.state.date.toLocaleTimeString()}</p> */}
+          <Clock />
         </header>
       </div>
     );
---
+++ src/Clock.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return <p>{this.state.date.toLocaleTimeString()}</p>;
+  }
+}
+
+export default Clock;
