a2d01bb8ce9200648e78fbaf2166ff635f068de6
th3d3v3lop3r <cjieren@gmail.com>
2023-07-28 11:05:08 +0800

    moved tick function to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import logo from "./logo.png";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{new Date().toString()}</p>
+          <p>{this.state.date.toLocaleTimeString()}</p>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,15 +4,4 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
-}
-
-setInterval(tick, 1000);
+root.render(<App />);
